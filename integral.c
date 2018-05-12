f9d54fa498b64edcb605d31fd7c36b231e2d13c9
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2018-05-12 10:31:54 +0300

    Added code, that calculates area. Fixed bug in integral()

--- integral.c
+++ integral.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 double integral(math_function f, double a, double b, double eps) {
-	unsigned n = 1000;
+	unsigned n = 20;
 	double first, second = (b - a) / 6 * (f(a) + 4 * f((b - a) / 2) + f(b));
 	do {
 		first = second;
@@ -12,7 +12,7 @@ double integral(math_function f, double a, double b, double eps) {
 		
 		double len = (b - a) / n;
 		for (int i = 0; i < n; i++) {
-			second += len / 6 * (f(i * len) + f(i * len + len) + 4 * f(i * len + len / 2));
+			second += len / 6 * (f(a + i * len) + f(a + i * len + len) + 4 * f(a + i * len + len / 2));
 		}
 	} while (1.0 / 15.0 * fabs(first - second) >= eps);
 	return second;
--- main.c
+++ main.c
@@ -30,48 +30,67 @@ static int cmp_double(const void *_l, const void *_r) {
 	return -1;
 }
 
-// structure describing function value in some point,
-// and what function has this value
+// structure describing functions' intersection point,
+// and what functions intersect in it
 typedef struct {
-		double y;
-		int f_num;
-} function_value;
+		double x;
+		int fnum_1, fnum_2;
+} function_inter;
 
 static int cmp_function_value(const void *l, const void *r) {
-	return cmp_double(&((function_value*)l)->y, &((function_value*)r)->y);
+	return cmp_double(&((function_inter*)l)->x, &((function_inter*)r)->x);
 }
 
 static double find_area(const double eps, bool print_inters, bool print_iter_count) {
-	double x[3];
-	x[0] = root(f1, f2, g1, g2, left_border, right_border, eps / 10);
+	function_inter x[3];
+	x[0].x = root(f1, f2, g1, g2, left_border, right_border, eps / 10);
+	x[0].fnum_1 = 0;
+	x[0].fnum_2 = 1;
 	if (print_iter_count) {
 		printf("Finding intersections of f1 and f2 took %u iterations.\n", last_root_call_num_steps);
 	}
-	x[1] = root(f1, f3, g1, g3, left_border, right_border, eps / 10);
+	x[1].x = root(f1, f3, g1, g3, left_border, right_border, eps / 10);
+	x[1].fnum_1 = 0;
+	x[1].fnum_2 = 2;
 	if (print_iter_count) {
 		printf("Finding intersections of f1 and f3 took %u iterations.\n", last_root_call_num_steps);
 	}
-	x[2] = root(f2, f3, g2, g3, left_border, right_border, eps / 10);
+	x[2].x = root(f2, f3, g2, g3, left_border, right_border, eps / 10);
+	x[2].fnum_1 = 1;
+	x[2].fnum_2 = 2;
 	if (print_iter_count) {
 		printf("Finding intersections of f2 and f2 took %u iterations.\n", last_root_call_num_steps);
 	}
 	if (print_inters) {
-		printf("f1 and f2 intersect at x = %.10f\n", x[0]);
-		printf("f1 and f3 intersect at x = %.10f\n", x[1]);
-		printf("f2 and f3 intersect at x = %.10f\n", x[2]);
+		printf("f1 and f2 intersect at x = %.10f\n", x[0].x);
+		printf("f1 and f3 intersect at x = %.10f\n", x[1].x);
+		printf("f2 and f3 intersect at x = %.10f\n", x[2].x);
 	}
 	qsort(x, 3, sizeof(x[0]), cmp_double);
-	double mid1 = (x[0] + x[1]) / 2, mid2 = (x[1] + x[2]) / 2;
-	// find upper and lower functions on [x_0; x_1]
-	function_value f_x[3];
-	f_x[0].y = f[0](mid1);
-	f_x[0].f_num = 0;
-	f_x[1].y = f[1](mid1);
-	f_x[1].f_num = 1;
-	f_x[2].y = f[2](mid1);
-	f_x[2].f_num = 2;
-	qsort(f_x, 3, sizeof(f_x[0]), cmp_function_value);
 	
+	// area on [x_0; x_1]
+	double seg1_f1_val = f[x[0].fnum_1]((x[0].x + x[1].x) / 2);
+	double seg1_f2_val = f[x[0].fnum_2]((x[0].x + x[1].x) / 2);
+	double f2_int1 = integral(f[x[0].fnum_2], x[0].x, x[1].x, eps / 10);
+	printf("Integrating %d from %.5f to %.5f...\n", x[0].fnum_1, x[0].x, x[1].x);
+	double f1_int1 = integral(f[x[0].fnum_1], x[0].x, x[1].x, eps / 10);
+	double seg1_area = f2_int1 - f1_int1;
+	printf("Segment 1[%.5f;%.5f] functions: %d %d\n", x[0].x, x[1].x, x[0].fnum_1, x[0].fnum_2);
+	printf("Integrals on segment 1: %.5f %.5f\n", f1_int1, f2_int1);
+	if (seg1_f1_val > seg1_f2_val) {
+		seg1_area = - seg1_area;
+	}
+	// area on [x_1; x_2]
+	double seg2_f1_val = f[x[2].fnum_1]((x[1].x + x[2].x) / 2);
+	double seg2_f2_val = f[x[2].fnum_2]((x[1].x + x[2].x) / 2);
+	double seg2_area = integral(f[x[2].fnum_2], x[1].x, x[2].x, eps / 10)
+					- integral(f[x[2].fnum_1], x[1].x, x[2].x, eps / 10);
+	printf("Segment 2[%.5f;%.5f] functions: %d %d\n", x[1].x, x[2].x, x[2].fnum_1, x[2].fnum_2);
+	if (seg2_f1_val > seg2_f2_val) {
+		seg2_area = - seg2_area;
+	}
+	printf("Area of segment 1: %.5f, segment 2: %.5f\n", seg1_area, seg2_area);
+	return seg1_area + seg2_area;
 }
 
 int main(int argc, char *argv[]) {
@@ -122,6 +141,7 @@ int main(int argc, char *argv[]) {
 	}
 	
 	double area = find_area(eps, print_intersections, print_iterations_count);
+	printf("%.5f\n", area);
 
 	return 0;
 }
